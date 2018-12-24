bb55a9b18abaa922917a29a26801df846f11eec1
taoly <taoly@hcmcloud.io>
2018-12-24 18:04:45 +0800

    python 3

--- lambda_demo.py
+++ lambda_demo.py
@@ -2,7 +2,7 @@
 
 def _lambda():
     _l = ['a', 'b', 'c']
-    # lambda 结合 map、filter、reduce使用
+    # lambda 结合 map、filter、reduce使用,返回list
     _l = map(lambda x: x + '!', _l)
     _r = ','.join(_l)
     return _r
