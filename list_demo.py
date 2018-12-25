c45c6e30a999750e6a837471b75ad506970780b0
taoly <taoly@hcmcloud.io>
2018-12-25 08:53:06 +0800

    python 3

--- list_demo.py
+++ list_demo.py
@@ -4,7 +4,47 @@
 def _enumerate():
     a = ["a", "b", "c"]
     for _i, _v in enumerate(a):
-        print({_i: _v})
+        print("enumerate:", {_i: _v})
 
 
 _enumerate()
+
+
+# map 返回list
+def _map():
+    _l = ['a', 'b', 'c']
+    # lambda 结合 map、filter、reduce使用,返回list
+    _l = map(lambda x: x + '!', _l)
+    _r = ','.join(_l)
+    return _r
+
+
+print("map:", _map())
+
+
+# 过滤返回符合条件的list
+def _filter():
+    _l = ['a', 'b', 'c']
+    _l = filter(lambda x: x == 'a', _l)
+    _r = ','.join(_l)
+    return _r
+
+
+print("filter:", _filter())
+
+
+# reduce() 函数会对参数序列中元素进行累积
+def _reduce():
+    _l = [{"v": 1}, {"v": 2}, {"v": 3}]
+
+    def _func(x, y):
+        _v = x['v'] + y['v']
+        # 把_func函数运算的结果作为下一次执行_func的x参数值
+        return dict(v=_v)
+
+    # python3 把reduce放到了functools中
+    from functools import reduce
+    return reduce(_func, _l)
+
+
+print("reduce:", _reduce())
